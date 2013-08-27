af3895bdceacc69621c25445189b897c49a312fb
Kevin <kevin_r_s_ch@hotmail.com>
2013-08-27 12:30:50 -0500

    leer tab simbolos y convertirla en lista d tuplas

--- Proyecto.hs
+++ Proyecto.hs
@@ -6,6 +6,10 @@ import Data.Char
 main::IO()
 main=do
    f <- readFile "testFile.c"
+   tablaSimb <- readFile "TablaDeSimbolos.txt"
+   --print(tablaSimb)
+   let tuplasTablaSimbolos = convertirTablaEnTuplas(tablaSimb)
+   print(tuplasTablaSimbolos)
    let str = "************* Analisis Lexico ************\n"
    writeFile "ResultadoAnalisis.txt" (str)
    print(str)
@@ -13,6 +17,27 @@ main=do
    appendFile "ResultadoAnalisis.txt" (f ++ "\n")   
    let arch = quitarIncludes(f)
    print(arch)
+
+
+convertirTablaEnTuplas::[Char]->[([Char],[Char])]
+convertirTablaEnTuplas lista = do
+	let tuplaToken = findFST(lista,"")
+	let tuplaLexeme = findSND(snd(tuplaToken),"")
+	if (null(lista)) then [] 
+	else [(fst(tuplaToken),fst(tuplaLexeme))]++convertirTablaEnTuplas(snd(tuplaLexeme))
+
+
+findFST::([Char],[Char])->([Char],[Char])
+findFST (lista,tk) = do
+	let token = tk ++ take 1 lista
+	if(head(lista)==',') then (init token,tail(lista))
+	else findFST(tail(lista),token)
+
+findSND::([Char],[Char])->([Char],[Char])
+findSND (lista,lex) = do
+	let lexeme = lex ++ take 1 lista
+	if(head(lista)=='\n') then (init lexeme,tail(lista))
+	else findSND(tail(lista),lexeme)
    
 quitarIncludes::[Char]->[Char]
 quitarIncludes lista = do
