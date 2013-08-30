d8c3e8d187a507f000690a23c746f5b9ca4544f8
Danny <dponce@espol.edu.ec>
2013-08-30 11:55:27 -0500

    Quitanto Comentarios Unilinea Y Multilinea

--- Proyecto.hs
+++ Proyecto.hs
@@ -17,6 +17,10 @@ main=do
    appendFile "ResultadoAnalisis.txt" (f ++ "\n")   
    let arch = quitarIncludes(f)
    print(arch)
+   let pc=quitarComentarioM(arch)
+   print(pc)
+   let un=quitarComentarioL(pc)
+   print(un)
 
 
 convertirTablaEnTuplas::[Char]->[([Char],[Char])]
@@ -56,3 +60,31 @@ quitaLinea lista = if(head(lista)=='\n') then quitarIncludes(tail(lista)) else q
 
 removeSpaces::[Char]->[Char]
 removeSpaces list = if (head(list)==' ' || head(list)=='\n') then removeSpaces(tail(list)) else list
+
+quitarComentarioM::[Char]->[Char]
+quitarComentarioM lista = do
+	let list = take 2 lista
+	if (null(lista)) then []
+	else if (list=="/*") then buscarCierreComentarioM(drop 2 lista)
+	else [head(lista)] ++ quitarComentarioM(tail(lista)) 
+
+buscarCierreComentarioM::[Char]->[Char]
+buscarCierreComentarioM lista = do
+	let list= take 2 lista
+	if (null(lista)) then []
+	else if (list=="*/") then quitarComentarioM(drop 2 lista)
+	else buscarCierreComentarioM(tail(lista))
+	
+quitarComentarioL::[Char]->[Char]
+quitarComentarioL lista = do
+	let list = take 2 lista
+	if (null(lista)) then []
+	else if (list=="//") then buscaCierreComentarioL(drop 2 lista)
+	else [head(lista)] ++ quitarComentarioL(tail(lista))
+	
+buscaCierreComentarioL::[Char]->[Char]
+buscaCierreComentarioL lista = do
+	let list = take 1 lista
+	if (null(lista)) then []
+	else if (list=="\n") then quitarComentarioL(drop 1 lista)
+	else buscaCierreComentarioL(tail(lista))
--- testFile.c
+++ testFile.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <conio.h>
 
+/*que tiro*/
+
 void main(){
+	/*asihfdajhdfhsgdf*/
+	//sdsds
 	int i;
+	//hola
 	for(i=0;i<0;i++){
 		printf("Hola Kevin");
 	}
+	/*cerrando
+	sjdshdjhdsjf
+	sddfdf*/
 	return 0;
 }
